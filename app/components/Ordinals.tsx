7da307449c6a1895a8267ac8b7fd31187359caff
estevanpedro <ewisoczynski@gmail.com>
2024-08-28 06:56:04 -0300

    feat: add json support handl end cases

--- app/components/Ordinals.tsx
+++ app/components/Ordinals.tsx
@@ -14,6 +14,23 @@ export const OrdinalRendering = ({ utxo }: { utxo: MempoolUTXO }) => {
 
   const contentType = ordinal?.contentType;
 
+  if (contentType?.includes("json")) {
+    return (
+      <JsonContent
+        url={`https://ordinals.com/content/${ordinal?.inscriptionId}`}
+      />
+    );
+  }
+
+  if (contentType?.includes("Bitcoin")) {
+    return (
+      <div>
+        <span>{ordinal?.utxo.satoshi} sats</span>
+        <span>{ordinal?.contentType}</span>
+      </div>
+    );
+  }
+
   if (contentType?.includes("text")) {
     return (
       <TextContent
@@ -68,10 +85,42 @@ const TextContent = ({ url }: TextContentProps) => {
   }, [url]);
 
   return (
-    <span className="text-[12px] max-w-[180px] text-center overflow-hidden">
+    <span className="px-2 text-[12px] max-w-[180px] text-center overflow-hidden">
       {text.replaceAll(`","`, `", "`)}
     </span>
   );
 };
 
-export default TextContent;
+export const JsonContent: React.FC<TextContentProps> = ({ url }) => {
+  const [text, setText] = useState<string>("");
+
+  useEffect(() => {
+    const fetchText = async () => {
+      try {
+        const response = await fetch(url);
+        if (response.ok) {
+          const contentType = response.headers.get("Content-Type");
+          if (contentType && contentType.includes("application/json")) {
+            const jsonData = await response.json();
+            setText(JSON.stringify(jsonData, null, 2)); // Pretty-print JSON
+          } else {
+            const textData = await response.text();
+            setText(textData);
+          }
+        } else {
+          console.error("Failed to fetch text content");
+        }
+      } catch (error) {
+        console.error("Error fetching text content:", error);
+      }
+    };
+
+    fetchText();
+  }, [url]);
+
+  return (
+    <pre className="px-2 text-[12px] max-w-[180px] text-center overflow-hidden whitespace-pre-wrap">
+      {text}
+    </pre>
+  );
+};
